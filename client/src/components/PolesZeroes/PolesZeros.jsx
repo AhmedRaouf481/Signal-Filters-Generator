232b843551701852fc052ffc31e2668a5b1fb5ef
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2023-02-02 18:22:10 +0200

    drag the point

--- client/src/components/PolesZeroes/PolesZeros.jsx
+++ client/src/components/PolesZeroes/PolesZeros.jsx
@@ -8,33 +8,77 @@ const PolesZeroes = () => {
     const [polesZeroesList, setPolesZeroesList] = useState([])
     const [point, setPoint] = useState({})
     const [type, setType] = useState(false)
+    const [isDraggable, setIsDraggable] = useState(false)
 
+    // Constants
+    const marginX = 7
+    const marginY = 14
+    const orgin = 182
+    const redius = 128
 
     // Methods
+
+    const mapCoordinate = (coordinate, margin, sign = 1) => {
+        coordinate = (coordinate + margin - orgin) / redius * sign
+        return coordinate
+    }
+
     const addPoint = (e) => {
         e.stopPropagation()
         setPoint({
             type,
-            x: e.clientX - 7,
-            y: e.clientY - 14
+            x: e.clientX - marginX,
+            y: e.clientY - marginY
         })
-        console.log(e.clientX)
-        console.log(e.clientY)
+        // console.log(e.clientX)
+        // console.log(e.clientY)
     }
 
     useEffect(() => {
         if (Object.keys(point).length !== 0 && polesZeroesList.indexOf(point) === -1) {
             setPolesZeroesList((list) => ([...list, point]))
-            console.log(polesZeroesList);
+            // console.log(polesZeroesList);
 
         }
     }, [point])
+    
+    useEffect(() => {
+        const zeroes = []
+        const poles = []
+        polesZeroesList.map((point,index) => {
+            const x = mapCoordinate(point.x,marginX)
+            const y = mapCoordinate(point.y,marginY,-1)
+            if(point.type === false){
+                zeroes.push({x,y})
+            }else{
+                poles.push({x,y})
+            }
+        })
+        console.log(zeroes);
+        console.log(poles);
+        console.log(polesZeroesList);
+    }, [polesZeroesList])
 
-    const handlePoint = (e) => {
+    const selectPoint = (e) => {
         e.stopPropagation();
-        console.log(e.currentTarget);
+        setIsDraggable(!isDraggable)
+        const myPoint = polesZeroesList[e.currentTarget.id]
+        myPoint.x = e.clientX - marginX
+        myPoint.y = e.clientY - marginY
     }
-    function rightClick(e) {
+
+    const dragPoint = (e) => {
+        e.stopPropagation();
+        if (isDraggable) {
+            e.currentTarget.style.left = e.clientX - marginX + "px"
+            e.currentTarget.style.top = e.clientY - marginY + "px"
+            // console.log(e.currentTarget.style.top);
+            // console.log("x", e.clientX)
+            // console.log("y", e.clientY)
+        }
+    }
+
+    const deletePoint = (e) => {
         e.stopPropagation();
         e.preventDefault();
         let list = [...polesZeroesList]
@@ -54,7 +98,7 @@ const PolesZeroes = () => {
                 onClick={addPoint}
             >
                 <div className="unit_circle"></div>
-                <div className="x-axis" ></div>
+                <div className="x-axis"></div>
                 <div className="y-axis"></div>
 
                 {
@@ -64,9 +108,9 @@ const PolesZeroes = () => {
                             id={index}
                             className={point.type ? 'pole' : 'zero'}
                             style={{ top: point.y, left: point.x }}
-                            onClick={handlePoint}
-                            onContextMenu={rightClick}
-                            draggable="true"
+                            onClick={selectPoint}
+                            onContextMenu={deletePoint}
+                            onMouseMove={dragPoint}
                         >
 
                             <FontAwesomeIcon
@@ -82,9 +126,9 @@ const PolesZeroes = () => {
                 className="custom-btn"
                 checked={type}
                 onlabel='Pole'
-                onstyle='danger'
+                onstyle='primary'
                 offlabel='Zero'
-                offstyle='primary'
+                offstyle='danger'
                 width='100'
                 style='mx-5'
                 onChange={() => {
