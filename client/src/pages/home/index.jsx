81fb56f2470e3db30ee5a38f71b4b01a0676dc67
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-02-03 19:28:33 -0800

    test component

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,14 +1,13 @@
 // import style from "./style.module.css";
 import PolesZeroes from "../../components/PolesZeroes/PolesZeros";
-
+import Test from "../../components/test";
 import FilterPlot from "../../components/filterPlot";
 import Graph from "../../components/graph/index";
 
 const Home = () => {
   return (
     <div>
-      <PolesZeroes />
-      {" "}
+      <PolesZeroes />{" "}
       <Graph
         x={[
           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
@@ -23,6 +22,7 @@ const Home = () => {
         color="grey"
       />
       <FilterPlot />
+      <Test />
     </div>
   );
 };
