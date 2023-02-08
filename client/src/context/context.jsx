63541965021db48d981ec1e30f772f67709ef44d
omaranwar21 <omarahmed11185@gmail.com>
2023-02-08 02:19:06 +0200

    all pass filter

--- client/src/context/context.jsx
+++ client/src/context/context.jsx
@@ -7,7 +7,7 @@ export const FileContextProvider = ({ children }) => {
 
   const [zeros, setZeros] = useState([
     [0, 1],
-    [1, 0],
+    [1, 0]
   ]);
   const [poles, setPoles] = useState([[0, 0]]);
   const [magPoints, setMagPoints] = useState([]);
